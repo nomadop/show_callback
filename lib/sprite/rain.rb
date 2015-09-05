098992a246ce40344a408a698f21b8328c0d5ab8
nomadop <nomadop@gmail.com>
2015-09-05 22:43:13 +0800

    remove rain and sun class

--- lib/sprite/rain.rb
+++
@@ -1,9 +0,0 @@
-class Rain < Ball
-  DEFAULT_CENTER_Y = 0
-  MIN_RAIDUS = 2
-  MAX_RAIDUS = 5
-
-  def initialize(center_x: 0.upto(World::WORLD_WIDTH).to_a.sample, center_y: DEFAULT_CENTER_Y, radius: (MIN_RAIDUS...MAX_RAIDUS).to_a.sample)
-    super(center_x, center_y, radius, Color.new(0, 100, 200))
-  end
-end
--- lib/sprite/sun.rb
+++
@@ -1,9 +0,0 @@
-class Sun < Ball
-  DEFAULT_CENTER_X = -150
-  DEFAULT_CENTER_Y = -100
-  DEFAULT_RADIUS = 150
-
-  def initialize(center_x: DEFAULT_CENTER_X, center_y: DEFAULT_CENTER_Y, radius: DEFAULT_RADIUS)
-    super(center_x, center_y, radius, Color::ORANGE)
-  end
-end
